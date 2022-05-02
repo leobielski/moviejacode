574bf600980d4eb13fa847ca25c40c1631fca9ec
leobielski <leobielski@yahoo.com.br>
2022-05-02 20:40:17 -0300

    code page Main

---
+++ src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+
+export default class Header extends Component {
+  render() {
+    return <div>Header</div>;
+  }
+}
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,41 +1,59 @@
 import React, { Component } from 'react';
 
-import { Container, Card, Row, Col, Button } from 'react-bootstrap';
+import { Container, Card, Row, Button } from 'react-bootstrap';
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
-      <>
-        <Container
-          style={{
-            paddingTop: '50',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            minWidth: '300',
-          }}
-        >
-          <h1 style={{ color: '#fff', fontFamily: 'ubuntu', marginTop: 20 }}>
-            Movie Now
-          </h1>
-          <Row>
-            <Card style={{ width: '18rem' }}>
-              <Card.Img
-                variant="top"
-                src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fdocplayer.nl%2F159044543-1-foodhallen-2-borrelen-3-feest-of-vergadering-4-onze-bars-5-foodstands.html&psig=AOvVaw2X2aLraV6ptZMs6ceXJBLN&ust=1651606299388000&source=images&cd=vfe&ved=2ahUKEwjmk8WHx8H3AhWOg5UCHbRuBLYQjRx6BAgAEAs"
-              />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Row>
-        </Container>
-      </>
+      <Container
+        style={{
+          paddingTop: '50',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          minWidth: '300',
+        }}
+      >
+        <h1 style={{ color: '#fff', fontFamily: 'ubuntu', marginTop: 20 }}>
+          Movie Now
+        </h1>
+        <Row style={{ display: 'flex', justifyContent: 'center' }}>
+          <Card style={{ width: 250, margin: 10 }}>
+            <Card.Img
+              variant="top"
+              src="https://i.pinimg.com/236x/80/10/ba/8010ba680f4d0d19ab2045232fd6470c.jpg"
+              style={{
+                width: 200,
+                height: 300,
+                alignSelf: 'center',
+                paddingTop: 20,
+                cursor: 'pointer',
+              }}
+            />
+            <Card.Body
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+              }}
+            >
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text style={{ fontSize: 12 }}>
+                Some quick example text to build on the card title and make up
+                the bulk of the cards content.
+              </Card.Text>
+              <Button style={{ width: '100%' }} variant="outline-dark">
+                Go somewhere
+              </Button>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
     );
   }
 }
